1e73db98a0035b1d4c4e8a80ba438bcd7c2cbc9d
isuchit <suchitrotti@gmail.com>
2020-09-04 01:40:59 +0530

    react-airbnb-clone

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <div className="header">
+            <Link to='/'>
+                <img className="header__icon" src="/airbnb-logo.png" alt="" />
+            </Link>
+            <div className="header__center">
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className="header__right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header
